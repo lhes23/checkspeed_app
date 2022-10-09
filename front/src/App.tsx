e5c200798c2a077dba100172f86fe90604ace8f5
Lester <32542078+lhes23@users.noreply.github.com>
2022-10-09 09:58:50 +0800

    add a rainbow effect and a loader

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react"
 import { motion } from "framer-motion"
 import client from "./axios/client"
 import ReactSpeedometer from "react-d3-speedometer"
+import Nav from "./components/Nav"
+import Loading from "./components/Loading"
 
 interface ISpeed {
   download: number
@@ -27,51 +29,65 @@ function App() {
   }
 
   return (
-    <div className="flex justify-center">
-      <div className="backdrop-blur-sm bg-white/30 w-full md:w-2/3 my-30 py-20 rounded-xl">
-        <div className="flex justify-center">
-          <h1 className="text-2xl md:text-4xl m-10 font-bold text-center">
-            Internet Speed Test
-          </h1>
-        </div>
-        <div className="flex justify-center">
-          <motion.button
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            onClick={runSpeedTestHandler}
-            className="bg-gradient-to-tr from-blue-400 to-green-300 text-white font-bold py-2 px-4 border-b-4 w-56 h-56 rounded-full"
-          >
-            <span className="text-2xl text-center">
-              {isLoading ? "Loading.." : "START"}
-            </span>
-          </motion.button>
-        </div>
-        {!isLoading && netSpeed && (
-          <div className="md:flex md:justify-evenly item-center">
-            <div className="flex justify-center">
-              <ReactSpeedometer
-                value={netSpeed?.download}
-                maxValue={100}
-                startColor="red"
-                endColor="green"
-                currentValueText={`Download Speed : ${netSpeed?.download} MBPS`}
-                textColor="#fff"
-              />
-            </div>
-            <div className="flex justify-center">
-              <ReactSpeedometer
-                value={netSpeed?.upload}
-                maxValue={100}
-                startColor="red"
-                endColor="green"
-                currentValueText={`Upload Speed : ${netSpeed?.upload} MBPS`}
-                textColor="#fff"
-              />
+    <>
+      <Nav />
+      <div className="flex justify-center">
+        <div className="w-full md:w-2/3 my-30 py-20 rounded-xl">
+          <div className="flex justify-center">
+            <div className=" justify-center">
+              <h1 className="text-white text-center my-4 text-3xl md:text-5xl opacity-75 font-bold leading-tight">
+                <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
+                  Internet Speed Test
+                </span>
+              </h1>
+              <p className="text-white leading-normal text-base md:text-2xl mb-8 text-center">
+                {isLoading
+                  ? "Now Checking..."
+                  : "Check your internet speed test here"}
+              </p>
             </div>
           </div>
-        )}
+          <div className="flex justify-center">
+            {isLoading ? (
+              <Loading />
+            ) : (
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                onClick={runSpeedTestHandler}
+                className="bg-gradient-to-r from-purple-800 to-green-500 text-white font-bold py-2 px-4 border-b-4 w-56 h-56 rounded-full hover:from-pink-500 hover:to-green-500 focus:ring"
+              >
+                <span className="text-2xl text-center">START</span>
+              </motion.button>
+            )}
+          </div>
+          {!isLoading && netSpeed && (
+            <div className="md:flex md:justify-evenly item-center my-10">
+              <div className="flex justify-center">
+                <ReactSpeedometer
+                  value={netSpeed?.download}
+                  maxValue={100}
+                  startColor="red"
+                  endColor="green"
+                  currentValueText={`Download Speed : ${netSpeed?.download} MBPS`}
+                  textColor="#fff"
+                />
+              </div>
+              <div className="flex justify-center">
+                <ReactSpeedometer
+                  value={netSpeed?.upload}
+                  maxValue={100}
+                  startColor="red"
+                  endColor="green"
+                  currentValueText={`Upload Speed : ${netSpeed?.upload} MBPS`}
+                  textColor="#fff"
+                />
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
